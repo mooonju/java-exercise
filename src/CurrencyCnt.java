494def1c03938915ec0801f023e1a11c73e9f5e2
MUNJU <mvv0030@gmail.com>
2022-10-04 14:47:32 +0900

    3. 몫 나머지 업데이트

--- src/CurrencyCnt.java
+++ src/CurrencyCnt.java
@@ -6,14 +6,30 @@ public class CurrencyCnt {
         int curr5000 = 5000;
         int curr1000 = 1000;
         int curr500 = 500;
+        int curr100 = 100;
+        int curr50 = 50;
+        int curr10 = 10;
 
-        int refund = 25000;
+        int refund = 25400;
 
-        System.out.printf("5만원권 %d 나머지:%d\n", refund / curr50000, refund%curr50000);
-        System.out.printf("만원권 %d 나머지:%d\n", refund / curr10000, refund%curr10000);
-        System.out.printf("5천원권 %d 나머지:%d\n", refund / curr5000, refund%curr5000);
-        System.out.printf("천원권 %d 나머지:%d\n", refund / curr1000, refund%curr1000);
-        System.out.printf("5백원권 %d 나머지:%d\n", refund / curr500, refund%curr500);
+        System.out.printf("5만원권 %d장 나머지:%d원\n", refund / curr50000, refund%curr50000);
+        // 25000원을 50000원으로
+        System.out.printf("1만원권 %d장 나머지:%d원\n", refund / curr10000, refund%curr10000);
+        // 5000원권은 1장만 나오게 하고 싶다. 왜냐하면 25000원에서 2만원은 만원짜리 두개로 처리했기 때문
+        // refund = refund - (curr10000 * 2);
+        // refund는 25000을 1원으로 나눈 나머지로 값을 업데이트한다
+        refund = refund % 10000; // modulo 나머지 mod %
+        System.out.printf("5천원권 %d장 나머지:%d원\n", refund / curr5000, refund%curr5000);
+        refund = refund % 5000;
+        System.out.printf("1천원권 %d장 나머지:%d원\n", refund / curr1000, refund%curr1000);
+        refund = refund % 1000;
+        System.out.printf("5백원권 %d개 나머지:%d원\n", refund / curr500, refund%curr500);
+        refund = refund % 500;
+        System.out.printf("1백원권 %d개 나머지:%d원\n", refund / curr100, refund%curr100);
+        refund = refund % 100;
+        System.out.printf("5십원 %d개 나머지:%d원\n", refund / curr50, refund%curr50);
+        refund = refund % 50;
+        System.out.printf("10원권 %d개 나머지:%d원\n", refund / curr10, refund%curr10);
 
     }
 }
