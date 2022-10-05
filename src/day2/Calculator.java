3fac2393d91b4466fbb29976e4f2c2abfcdf07bd
MUNJU <mvv0030@gmail.com>
2022-10-05 10:49:21 +0900

    생성자 추가

--- src/day2/Calculator.java
+++ src/day2/Calculator.java
@@ -1,17 +1,27 @@
 package day2;
 
 public class Calculator {
-    public void plus(int a, int b){
+    private  int a;
+    private  int b;
+
+    // 생성자 단축키 alt + insert
+
+    // 생성자 추가하면서 매개변수 없애기
+    public Calculator(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public void plus(){
         System.out.println(a + b);
     }
-    public void minus(int a, int b){
+    public void minus(){
         System.out.println(a - b);
     }
-    public void multiple(int a, int b){
+    public void multiple(){
         System.out.println(a * b);
     }
-    public void divide(int a, int b){
-        float result = (float) a / b;
-        System.out.println(result);
+    public void divide(){
+        System.out.println((float) a / b);
     }
 }
--- src/day2/CalculatorMain.java
+++ src/day2/CalculatorMain.java
@@ -1,14 +1,12 @@
 package day2;
 
-public class CalculatorMain {
+    public class CalculatorMain {
     public static void main(String[] args) {
-        int a = 9;
-        int b = 3;
 
-        Calculator calculator = new Calculator();
-        calculator.plus(a, b);
-        calculator.minus(a, b);
-        calculator.multiple(a, b);
-        calculator.divide(a, b);
+        Calculator calculator = new Calculator(10, 20);
+        calculator.plus();
+        calculator.minus();
+        calculator.multiple();
+        calculator.divide();
     }
 }
