527784fa1c89c005ae80a1aa55666221f98d9683
MUNJU <mvv0030@gmail.com>
2022-10-05 23:11:08 +0900

    20221004 과제 codeup 1001-1010

---
+++ src/codeup/hw1004/codeup_1005.java
@@ -0,0 +1,7 @@
+package codeup.hw1004;
+
+public class codeup_1005 {
+    public static void main(String[] args) {
+        System.out.println("\"" + "Hello World" + "\"");
+    }
+}
---
+++ src/codeup/hw1004/codeup_1010.java
@@ -0,0 +1,11 @@
+package codeup.hw1004;
+
+import java.util.Scanner;
+
+public class codeup_1010 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println(num);
+    }
+}
