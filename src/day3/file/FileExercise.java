f3ccd0e6494e489512321af7c2d416d3ba897dcb
MUNJU <mvv0030@gmail.com>
2022-10-06 15:50:45 +0900

    파일 N글자 출력

--- src/day3/file/FileExercise.java
+++ src/day3/file/FileExercise.java
@@ -1,11 +1,31 @@
 package day3.file;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class FileExercise {
+
+    private  String filename;
+
+    public FileExercise(String filename) {
+        this.filename = filename;
+    }
+
+    public String readNChars(int N) throws IOException{
+        FileReader fileReader = new FileReader(this.filename);
+        String str = "";
+        for (int i = 0; i < N; i++){
+            // - 가 나올 경우 break
+            int asciiCd = fileReader.read();
+            if(asciiCd == -1){
+                return str;
+            }
+            str += (char) asciiCd;
+        }
+        return str;
+    }
+
     public void printFiles(){
         File dir = new File("./");
         File files[] = dir.listFiles();
@@ -40,13 +60,16 @@ public class FileExercise {
 
     }
     public static void main(String[] args) throws IOException {
-        FileExercise fileExercise = new FileExercise();
+        FileExercise fileExercise = new FileExercise("./aa_file.txt");
         char c = fileExercise.readAChar("./aa_file.txt");
         String str = fileExercise.read2Chars("./aa_file.txt");
         System.out.println(c);
         System.out.println(str);
+        System.out.println(fileExercise.readNChars(10));
         /*
             1
+            11
+            1111222345
         */
     }
 }
