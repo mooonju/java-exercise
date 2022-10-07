35c7d4668217f9f926d2aa6ae0c92f1a8ea44739
MUNJU <mvv0030@gmail.com>
2022-10-07 16:37:45 +0900

    20221005 과제 codeup 1011-1020

--- src/codeup/hw1005/codeup_1019.java
+++ src/codeup/hw1005/codeup_1019.java
@@ -6,7 +6,7 @@ public class codeup_1019 {
     public static void main(String[] args) {
         // 연월일 입력받아 그대로 출력하기
         Scanner sc = new Scanner(System.in);
-        String date[] = sc.next().split("\\.");
+        String[] date = sc.next().split("\\.");
         // split 사용시 '.'을 기준으로 분리해야 하는 경우 [.] 또는 \\. 사용해서 분리 필요
 
         int year = Integer.parseInt(date[0]);
--- src/codeup/hw1005/codeup_1020.java
+++ src/codeup/hw1005/codeup_1020.java
@@ -6,7 +6,8 @@ public class codeup_1020 {
     public static void main(String[] args) {
         // 주민번호 입력받아 형태 바꿔 출력하기
         Scanner sc = new Scanner(System.in);
-        String number[] = sc.next().split("-");
+        String[] number = sc.next().split("-");
+        char a = sc.next().charAt(0);
 
         String num1 = number[0];
         String num2 = number[1];
