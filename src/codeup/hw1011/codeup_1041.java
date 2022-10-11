2553beb1e90748a97aafc5cb038488426cfdf4f2
MUNJU <mvv0030@gmail.com>
2022-10-12 00:27:08 +0900

    20221011 과제 codeup 1041-1050

--- src/codeup/hw1011/codeup_1041.java
+++ src/codeup/hw1011/codeup_1041.java
@@ -6,7 +6,7 @@ public class codeup_1041 {
     public static void main(String[] args) {
         // 문자 1개 입력받아 다음 문자 출력하기
         Scanner sc = new Scanner(System.in);
-        char ch = sc.next().charAt(1);
+        char ch = sc.next().charAt(0);
         System.out.println((char)(ch+1));
     }
 }
