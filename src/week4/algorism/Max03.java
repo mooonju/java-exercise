9a8e3fe1492de7fe194b24bdb3821fb27c08a777
MUNJU <mvv0030@gmail.com>
2022-10-12 10:14:45 +0900

    20221011 알고리즘 최대값 구하기(리팩토링)

---
+++ src/week4/algorism/Max03.java
@@ -0,0 +1,34 @@
+package week4.algorism;
+
+public class Max03 {
+    // 리팩토링
+    public int getMax(int[] arr){
+        // 비교할 변수 선언
+        // arr이 모두 음수인 경우 0으로 하면 0이 max이기 때문에
+        // arr[0]으로 초기화
+        int maxValue = arr[0];
+        // arr[0]과 arr[1]부터 비교할 수 있도록 i = 1;
+        for(int i = 1; i < arr.length; i++){
+            if(arr[i] > maxValue){
+                maxValue = arr[i];
+            }
+        }
+        return maxValue;
+    }
+
+    public static void main(String[] args) {
+        // arr 선언
+        int[] arr = new int[]{-3, -29, -38, -12, -57, -74, -40, -85, -61};
+        // loop 구성 (전체 순회)
+        Max03 max03 = new Max03();
+        int result = max03.getMax(arr);
+        // 테스트 코드
+        if(result == -3){
+            System.out.println("테스트 통과");
+        }else{
+            System.out.println("테스트 실패 result: " + result);
+        }
+
+
+    }
+}
