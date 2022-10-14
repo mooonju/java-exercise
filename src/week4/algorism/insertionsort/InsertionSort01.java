2d819a0ec7cdfffeca5f1027a52c283850d21637
MUNJU <mvv0030@gmail.com>
2022-10-14 09:48:40 +0900

    swap

--- src/week4/algorism/insertionsort/InsertionSort01.java
+++ src/week4/algorism/insertionsort/InsertionSort01.java
@@ -5,9 +5,14 @@ import java.util.Arrays;
 public class InsertionSort01 {
     public int[] sort(int[] arr) {
         int i = 1;
+        if (arr[i] < arr[i-1]) { // arr[1] vs arr[0]
+            // swap
+            int temp = arr[i-1];
+            arr[i-1] = arr[i];
+            arr[i] = temp;
+        }
 
-
-        return new int[]{};
+        return arr;
     }
 
     public static void main(String[] args) {
