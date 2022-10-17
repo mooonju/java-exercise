7baa65abedec454e85797b4d7db47b898a827f43
MUNJU <mvv0030@gmail.com>
2022-10-17 09:23:09 +0900

    20221017 알고리즘 별찍기 2

--- src/week5/algorism/RightTriangle.java
+++ src/week5/algorism/RightTriangle.java
@@ -1,17 +1,27 @@
 package week5.algorism;
 
 public class RightTriangle {
+    public void star(int num) {
+        for (int i=0; i<num; i++){
+            // for문을 하나 더 쓰고
+            // println 말고 print, printf 등을 써서 한줄로 출력
+            for (int j=0; j<=i; j++){
+                System.out.printf("* ");
+            }
+            System.out.printf("\n");
+        }
+    }
+
     public static void main(String[] args) {
         /*
-        *
-        * *
-        * * *
-        * * * *
+        *               i = 0 별이 1개
+        * *             i = 1 별이 2개
+        * * *           i = 2 별이 3개
+        * * * *         i = 3 별이 4개
         */
-        System.out.println("*");
-        System.out.println("* *");
-        System.out.println("* * *");
-        System.out.println("* * * *");
+
+        RightTriangle rightTriangle = new RightTriangle();
+        rightTriangle.star(10);
 
     }
 }
