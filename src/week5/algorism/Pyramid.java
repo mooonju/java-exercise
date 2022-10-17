8db77599de5d3df94894286f4c03fc155726727c
MUNJU <mvv0030@gmail.com>
2022-10-17 10:14:18 +0900

    20221017 알고리즘 피라미드 별찍기

--- src/week5/algorism/Pyramid.java
+++ src/week5/algorism/Pyramid.java
@@ -11,10 +11,10 @@ public class Pyramid {
 
     public  void printStar(int n) {
         for (int i = 0; i < n; i++) {
-            for (int j = n; j > i; j--) {
+            for (int j = 1; j < n-i; j++) {
                 System.out.printf(" ");
             }
-            for (int k = i+1; k > 0; k--) {
+            for (int k = 0; k < i+1; k++) {
                 System.out.printf("* ");
             }
             System.out.println();
