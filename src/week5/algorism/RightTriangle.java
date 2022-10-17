6c67a0a831d37c6e70893ef75b7c496fbdd9eb26
MUNJU <mvv0030@gmail.com>
2022-10-17 09:09:23 +0900

    20221017 알고리즘 별찍기 1

---
+++ src/week5/algorism/RightTriangle.java
@@ -0,0 +1,17 @@
+package week5.algorism;
+
+public class RightTriangle {
+    public static void main(String[] args) {
+        /*
+        *
+        * *
+        * * *
+        * * * *
+        */
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
+
+    }
+}
