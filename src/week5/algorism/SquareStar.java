b968ecd27b90ec50798fa8c58b3874f2b9a25d22
MUNJU <mvv0030@gmail.com>
2022-10-18 09:23:11 +0900

    20221018 알고리즘 직사각형 별찍기

--- src/week5/algorism/SquareStar.java
+++ src/week5/algorism/SquareStar.java
@@ -8,12 +8,20 @@ public class SquareStar {
             }
             System.out.println();
         }
+    }
 
+    public void printRectangle(int x, int y) {
+        for (int i = 0; i < y; i++) {
+            for (int j = 0; j < x; j++) {
+                System.out.printf("*");
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
         SquareStar squareStar = new SquareStar();
         squareStar.printSquare(4);
-
+        squareStar.printRectangle(5,3);
     }
 }
