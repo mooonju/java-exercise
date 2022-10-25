dee99f4324503974f35fe7ad6c5a186b9c4f2e01
MUNJU <mvv0030@gmail.com>
2022-10-25 10:20:27 +0900

    Hash

---
+++ src/main/java/algorism/hash/HashFunction.java
@@ -0,0 +1,16 @@
+package algorism.hash;
+
+public class HashFunction {
+    public int hash(String key) {
+        for (int i = 0; i < key.length(); i++) {
+            char c = key.charAt(i);
+            System.out.println(c);
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        HashFunction hf = new HashFunction();
+        hf.hash("moooon");
+    }
+}
