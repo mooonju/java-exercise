c8bb79ebd91a281b4c961a4749f6b27dff27c554
MUNJU <mvv0030@gmail.com>
2022-11-03 09:33:10 +0900

    [알고리즘] 에라토스테네스 체 1단계 배열 만들기

---
+++ src/main/java/algorism/primenumber/PrimeNum2.java
@@ -0,0 +1,21 @@
+package algorism.primenumber;
+
+public class PrimeNum2 {
+
+    public int solution(int n) {
+        boolean[] checks = new boolean[n+1];
+        return 0;
+    }
+
+    public static void main(String[] args) {
+//        2, 2
+//        3, 4
+//        4, 6
+//        5, 8
+//      출력하기
+        PrimeNum2 pn = new PrimeNum2();
+        System.out.println(pn.solution(10));
+
+
+    }
+}
