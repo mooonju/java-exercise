38978a24046ba4c2c9100c7228eaf5ca0bc2b45b
mooonju <mvv0030@gmail.com>
2022-12-09 09:19:17 +0900

    [알고리즘] dp의 loop 구성

---
+++ src/main/java/algorism/dp/LCS2.java
@@ -0,0 +1,19 @@
+package algorism.dp;
+
+public class LCS2 {
+    public static void main(String[] args) {
+        String str1 = "ABCDCBA"; // 열
+        String str2 = "DCABDC"; // 행
+
+        // str2인 DCABDC를 i로 놓고 바깥쪽에서 반복할 예정 - 행
+        // str1인 ABCDCBA를 j로 놓고 안쪽에서 반복할 예정 - 열
+
+        int[][] dp = new int[str2.length()][str1.length()];
+
+        for (int i = 0; i < str2.length(); i++) {
+            for (int j = 0; j < str1.length(); j++) {
+                System.out.printf("%s %s\n", str2.charAt(i), str1.charAt(j));
+            }
+        }
+    }
+}
